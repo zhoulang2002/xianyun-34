004a6eab79ffb23db73d0ab8235eff33060325eb
zhoulang2002 <52401753+zhoulang2002@users.noreply.github.com>
2019-07-30 12:24:35 +0800

    123

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+
+
+  </div>
+</template>
+
+<script>
+
+import Header from @/components/header;
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/air/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>卖飞机票的</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    
+</style>
