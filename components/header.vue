5e8dbb1399a66ba71abd675c9059dec3466bb566
zhoulang2002 <52401753+zhoulang2002@users.noreply.github.com>
2019-07-31 12:09:16 +0800

    123

--- components/header.vue
+++ components/header.vue
@@ -1,152 +1,147 @@
 <template>
     <header class="header">
-        <el-row type="flex" justify="space-between" class="main"> 
-
+        <el-row type="flex" justify="space-between" class="main">
             <!-- logo -->
-             <div class="logo">
-                <muxt-link to="/">
+            <div class="logo">
+                <nuxt-link to="/">
                     <img src="http://157.122.54.189:9093/images/logo.jpg" alt="">
-                </muxt-link>
-            </div> 
+                </nuxt-link>
+            </div>
 
-            <!-- 菜单栏  -->
-             <el-row type="flex" class="navs">
+            <!-- 菜单栏 -->
+            <el-row type="flex" class="navs">
                 <nuxt-link to="/">首页</nuxt-link>
                 <nuxt-link to="/post">旅游攻略</nuxt-link>
                 <nuxt-link to="/hotel">酒店</nuxt-link>
                 <nuxt-link to="/air">国内机票</nuxt-link>
-            </el-row> 
+            </el-row>
 
-            <!-- 登录/用户信息 -->
-            <!-- <el-row type="flex" class="middle"> -->
+            <!-- 登录 用户信息 -->
+            <el-row type="flex" align="midde">
 
                 <!-- 如果用户存在则展示用户信息，用户数据来自store -->
-                <el-dropdow v-if="false">
-                    <el-row type="flex" align="middle" class="el-dropdown-lin">
+                <el-dropdown v-if="false">
+                    <el-row type="flex" align="middle" class="el-dropdown-link">
                         <nuxt-link to="#">
-                            <img src="http://157.122.54.189:9093/images/pic)sea.jpeg/">用户名
+                            <img src="http://157.122.54.189:9093/images/pic_sea.jpeg"/>
+                            用户名
                         </nuxt-link>
-                        <i class="el-iocn-caret-bottom el-icon--right"></i>
+                        <i class="el-icon-caret-bottom el-icon--right"></i>
                     </el-row>
                     <el-dropdown-menu slot="dropdown">
                         <el-dropdown-item>
                             <nuxt-link to="#">个人中心</nuxt-link>
                         </el-dropdown-item>
                         <el-dropdown-item>
-                            <div @click="hamdleLogout">退出</div>
+                            <div @click="handleLogout">退出</div>
                         </el-dropdown-item>
                     </el-dropdown-menu>
-                </el-dropdow> 
+                </el-dropdown>
 
-                <!-- 不存在用户信息展示登录注册链接  -->
-                 <nuxt-link to="/user/login" class="account-link" v-else>
-                登录 / 注册
+                <!-- 不存在用户信息展示登录注册链接 -->
+                <nuxt-link to="/user/login" class="acconut-link" v-else>
+                    登录 / 注册
                 </nuxt-link>
             </el-row>
         </el-row>
     </header>
-</header>
 </template>
-
 <script>
 export default {
     methods: {
         // 用户退出
-        handleLogout(){
-
-        }
+        handleLogout(){},
     }
 }
 </script>
 
-<style <style lang="less" scoped>
-     .header{
-         height: 60px;
-         line-height:60px;
-         background: #fff;
-         border-bottom: 1px #ddd solid;
-         box-shadow:0 3px 0 #f5f5f5;
-         box-sizing: border-box;
+<style scoped lang="less">
+    .header{
+        height: 60px;
+        line-height: 60px;
+        background: #fff;
+        border-bottom: 1px #ddd solid;
+        box-shadow: 0 3px 0 #f5f5f5;
+        box-sizing: border-box;
 
-         .main{
-             width:1000px;
-             margin:0 auto;
-         }
+        .main{
+            width:1000px;
+            margin: 0 auto;
+        }
 
-         .logo{
-             width:156px;
-             padding-top:8px;
+        .logo{
+            width:156px;
+            padding-top:8px;
 
-             img{
-                 display: block;
-                 width:100%;
-             }
-         }
+            img{
+                display: block;
+                width:100%;
+            }
+        }
 
-         .navs{
-             margin: 0 20px;
-             flex:1;
+        .navs{
+            margin: 0 20px;
+            flex:1;
 
-             a{
-                 display:block;
-                 padding: 0 20px;
-                 height:60px;
-                 box-sizing: border-box;
+            a{
+                display: block;
+                padding: 0 20px;
+                height: 60px;
+                box-sizing: border-box;
 
-                 &:hover,&:focus, &:active {
-                     border-bottom:5px #409eff solid;
-                     color:#409eff;
-                 }
-             }
+                &:hover,&:focus,&:active {
+                    border-bottom: 5px solid #409eff;
+                    color: #409eff;
+                }
+            }
+            /deep/ .nuxt-link-exact-active{
+                background: #409eff;
+                color:#fff!important;
+            }
+        }
 
-              .nuxt-link-exact-active{
-                 background: #409eff;
-                 color: #fff!important;
-             }
-         }
+        .message{
+            height: 36px;
+            line-height: 1;
+            cursor: pointer;
+            .el-icon-bell{
+                margin-right: 2px;
+                font-size: 18px;
+            }
+        }
 
-         .message{
-             height:36px;
-             line-height:1;
-             cursor:pointer;
-             .el-icon-bell{
-                 margin-right:2px;
-                 font-size:18px;
-             }
-         }
+        .el-dropdown-link{
+            margin-left:20px;
 
-         .el-dropdow-link{
-             margin-left: 20px;
+            &:hover{
+                img{
+                    border-color: #409eff;
+                }
+            }
 
-             &:hover{
-                 img{
-                     border-color: #409eff;
-                 }
-             }
+            a{
+                display: block;
+            }
 
-             a{
-                 display: block;
-             }
+            img{
 
-             img{
+                width:32px;
+                height: 32px;
+                vertical-align: middle;
+                border: 2px #fff solid;
+                border-radius: 50px;
+            }
+        }
 
-                 width:32px;
-                 height:32px;
-                 vertical-align: middle;
-                 border:2px #fff solid;
-                 border-radius: 50px;
-             }
-         }
+        .account-link{
+            font-size: 14px;
+            margin-left: 10px;
+            color: #666;
+        }
 
-         .account-link{
-             font-size: 14px;
-             margin-left: 10px;
-             color:#666;
-
-             &:hover{
-                 color:#409eff;
-                 text-decoration: underline;
-             }
-         }
-     }
+        &:hover{
+            color:#409eff;
+            text-decoration: underline;
+        }
+    }
 </style>
---
+++ store/user.js
@@ -0,0 +1,21 @@
+// 保存数据的属性 state
+export const state = {
+    // 用户信息
+    userInfo: {
+        token: "",
+        user: {
+            nickname: "沐颜"
+        }
+    }
+}
+
+
+// 同步修改数据 mutations
+export const mutations = {
+
+}
+
+// 异步修改数据 actions
+export const actions = {
+    
+}
